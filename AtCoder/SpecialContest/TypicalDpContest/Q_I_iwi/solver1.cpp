33dfa7dc26d43c9c57d53f84c5c4c54ee6c39045
uetsu840 <vd7t-ktgw@asahi-net.or.jp>
2019-05-01 15:07:46 +0900

    solved some dp problems

---
+++ AtCoder/SpecialContest/TypicalDpContest/Q_I_iwi/solver1.cpp
@@ -0,0 +1,225 @@
+#pragma GCC optimize ("O3")
+
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+#include <set>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+using QWORD  = uint64_t;
+using SQWORD = int64_t;
+using DWORD  = uint32_t;
+using SDWORD = int32_t;
+using WORD   = uint16_t;
+using SWORD  = int16_t;
+using BYTE   = uint8_t;
+using SBYTE  = int8_t;
+using DOUBLE = double;
+using FLOAT  = float;
+
+#define MIN_SDWORD (-2147483648)
+#define MAX_SDWORD (2147483647)
+#define MIN_SBYTE (-128)
+#define MAX_SBYTE (127)
+
+#define MIN_SQWORD (0x8000000000000000)
+#define MAX_SQWORD (0x7FFFFFFFFFFFFFFF)
+
+#define MAX_QWORD  (0xFFFFFFFFFFFFFFFF)
+#define MAX_DWORD  (0xFFFFFFFF)
+#define MAX_WORD   (0xFFFF)
+#define MAX_BYTE   (0xFF)
+
+
+#define ArrayLength(a)  (sizeof(a) / sizeof(a[0]))
+
+static inline QWORD MAX(QWORD a, QWORD b) { return a > b ? a : b; }
+static inline DWORD MAX(DWORD a, DWORD b) { return a > b ? a : b; }
+static inline SDWORD MAX(SDWORD a, SDWORD b) { return a > b ? a : b; }
+static inline QWORD MIN(QWORD a, QWORD b) { return a < b ? a : b; }
+static inline DWORD MIN(DWORD a, DWORD b) { return a < b ? a : b; }
+static inline SDWORD MIN(SDWORD a, SDWORD b) { return a < b ? a : b; }
+
+#define BYTE_BITS   (8)
+#define WORD_BITS   (16)
+#define DWORD_BITS  (32)
+#define QWORD_BITS  (64)
+
+using M_BOOL = bool;
+#define M_TRUE (true)
+#define M_FALSE (false)
+#define DIVISOR (1000000007)
+
+static inline void inputString(char *pcStr)
+{
+    char *pcCur = pcStr;
+    for (;;) {
+        char c = getchar();
+        if (('\n' == c) || (EOF == c)) {
+            break;
+        }
+        *pcCur = c;
+        pcCur++;
+    }
+    *pcCur = '\0';
+}
+
+
+static inline SQWORD inputSQWORD(void)
+{
+    SQWORD sqNumber = 0;
+    SQWORD sqMultiplier = 1;
+    M_BOOL bRead = M_FALSE;
+    for (;;) {
+        char c = getchar();
+        if (!bRead) {
+            if ('-' == c) {
+                sqMultiplier = -1;
+            }
+        }
+        if (('0' <= c) && (c <= '9')) {
+            sqNumber *= 10LL;
+            sqNumber += (SQWORD)(c - '0');
+            bRead = M_TRUE;
+        } else {
+            if (bRead) {
+                return sqNumber * sqMultiplier;
+            }
+        }
+    }
+}
+
+
+static inline SDWORD inputSDWORD(void)
+{
+    SDWORD lNumber = 0;
+    SDWORD lMultiplier = 1;
+    M_BOOL bRead = M_FALSE;
+    for (;;) {
+        char c = getchar();
+        if (!bRead) {
+            if ('-' == c) {
+                lMultiplier = -1;
+            }
+        }
+        if (('0' <= c) && (c <= '9')) {
+            lNumber *= 10;
+            lNumber += (c - '0');
+            bRead = M_TRUE;
+        } else {
+            if (bRead) {
+                return lNumber * lMultiplier;
+            }
+        }
+    }
+}
+
+static inline DOUBLE inputFP(void)
+{
+    DOUBLE dInt = 0.0;
+    DOUBLE dFrac = 0.0;
+    DOUBLE dMultiplier = 1.0;
+    DWORD dwFpCnt = 0;
+    DOUBLE *pdCur = &dInt;
+    M_BOOL bRead = M_FALSE;
+    for (;;) {
+        char c = getchar();
+        if (!bRead) {
+            if ('-' == c) {
+                dMultiplier = -1;
+            }
+        }
+        if ('.' == c) {
+            pdCur = &dFrac;
+        } else if (('0' <= c) && (c <= '9')) {
+            (*pdCur) *= 10;
+            (*pdCur) += (DOUBLE)(c - '0');
+            bRead = M_TRUE;
+            if (pdCur == &dFrac) {
+                dwFpCnt++;
+            }
+        } else {
+            if (bRead) {
+                return dMultiplier * (dInt + dFrac / (pow((DOUBLE)10.0 , (DOUBLE)dwFpCnt)));
+            }
+        }
+    }
+}
+
+#define MAX_INPUT   (300)
+
+int main(void)
+{
+    char acInput[MAX_INPUT + 1];
+    inputString(acInput);
+
+    vector<SDWORD> vpcStrTop;
+    
+    /* 
+    *   wが2個並んでいるところをヌル文字で置き換え、先頭のiの位置を記憶してゆく
+    *       先頭の 'w' と、末尾の 'w' も取り除く。
+    *    
+    * 
+    */
+    SDWORD lStrLen = strlen(acInput);
+
+    bool bSep = true;
+    for (SDWORD lIdx = 0; lIdx < lStrLen; lIdx++) {
+        if ('w' == acInput[lIdx]) {
+            SDWORD lContCnt = 0;
+            while ('w' == acInput[lIdx + lContCnt]) {
+                lContCnt++;
+            }
+            if (2 <= lContCnt) {
+                for (SDWORD lSepIdx = 0; lSepIdx < lContCnt; lSepIdx++) {
+                    acInput[lIdx + lSepIdx] = '\0';
+                }
+            }
+        }
+    }
+    if ('w' == acInput[0]) {
+        acInput[0] = '\0';
+    }
+    if ('w' == acInput[lStrLen - 1]) {
+        acInput[lStrLen - 1] = '\0';
+    }
+   
+    if ('i' == acInput[0]) {
+        vpcStrTop.emplace_back(0);
+    }
+    for (SDWORD lIdx = 1; lIdx < lStrLen; lIdx++) {
+        if (('i' == acInput[lIdx]) && ('\0' == acInput[lIdx - 1])) {
+            vpcStrTop.emplace_back(lIdx);
+        }
+    }
+
+    /* 各文字列でiとwを数える */
+    SDWORD lAns = 0;
+    for (auto str:vpcStrTop) {
+        char *pC = &(acInput[str]);
+        SDWORD lWCnt = 0;
+        SDWORD lICnt = 0;
+        while (*pC != '\0') {
+            if ('i' == *pC) {
+                lICnt++;
+            }
+            if ('w' == *pC) {
+                lWCnt++;
+            }
+            pC++;
+        }
+        lAns += min(lICnt/2, lWCnt);
+    }
+    printf("%d\n", lAns);
+
+    return 0;
+}
