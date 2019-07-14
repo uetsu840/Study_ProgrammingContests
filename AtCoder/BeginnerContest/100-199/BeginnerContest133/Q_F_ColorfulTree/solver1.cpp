3599f7d32c09f6ffcd4203bd7ead77b7c995e598
uetsu840 <vd7t-ktgw@asahi-net.or.jp>
2019-07-14 13:59:27 +0900

    codeforces round 573

--- AtCoder/BeginnerContest/100-199/BeginnerContest133/Q_F_ColorfulTree/solver1.cpp
+++ AtCoder/BeginnerContest/100-199/BeginnerContest133/Q_F_ColorfulTree/solver1.cpp
@@ -55,11 +55,6 @@ static inline QWORD MIN(QWORD a, QWORD b) { return a < b ? a : b; }
 static inline DWORD MIN(DWORD a, DWORD b) { return a < b ? a : b; }
 static inline SDWORD MIN(SDWORD a, SDWORD b) { return a < b ? a : b; }
 
-static inline bool DoubleIsZero(const DOUBLE &a)
-{
-    return abs(a) < DOUBLE_EPS;
-}
-
 #define BYTE_BITS   (8)
 #define WORD_BITS   (16)
 #define DWORD_BITS  (32)
@@ -224,8 +219,184 @@ static SQWORD combMod(SQWORD n, SQWORD k)
 
 /*----------------------------------------------*/
 
+struct EDGE_ST {
+    SQWORD sqTo;
+    SQWORD sqCost;
+    SQWORD sqColor;
+
+    EDGE_ST(SQWORD to, SQWORD color, SQWORD cost) {
+        sqTo = to;
+        sqColor = color;
+        sqCost = cost;
+    };
+};
+
+#define N_MAX_NODES     (100000)
+#define MAX_LOG_NODES   (20)
+
+struct TREE {
+    vector<EDGE_ST> avecstEdge[N_MAX_NODES + 1];
+    SQWORD sqNumNode;
+    SQWORD aasqParent[MAX_LOG_NODES][N_MAX_NODES + 1];
+    SQWORD asqDepth[N_MAX_NODES + 1];
+    SQWORD asqCost[N_MAX_NODES + 1];        /* rootからのコスト */
+
+    TREE(SQWORD sqN) {
+        sqNumNode = sqN;
+    }
+
+    void retistEdge(SQWORD sqA, SQWORD sqB, SQWORD sqColor, SQWORD sqCost)
+    {
+        avecstEdge[sqA].emplace_back(sqB, sqColor, sqCost);
+        avecstEdge[sqB].emplace_back(sqA, sqColor, sqCost);
+    }
+
+    void dfs(SQWORD sqNode, SQWORD sqP = -1, SQWORD sqDepth = 0, SQWORD sqCost = 0)
+    {
+        aasqParent[0][sqNode] = sqP;
+        asqDepth[sqNode] = sqDepth;
+        asqCost[sqNode] = sqCost;
+        for (auto edge:avecstEdge[sqNode]) {
+            if (edge.sqTo != sqP) {
+                dfs(edge.sqTo, sqNode, sqDepth + 1, sqCost + edge.sqCost);
+            }
+        }
+    }
+
+    /**
+     *  initialize
+     */    
+    void init(SQWORD sqRoot) 
+    {
+        /* measure Depth */
+        dfs(sqRoot);
+
+        /* initialize parent */
+        for (SQWORD sqLogIdx = 0; sqLogIdx + 1 < MAX_LOG_NODES; sqLogIdx++) {
+            for (SQWORD sqNode = 1; sqNode <= sqNumNode; sqNode++) {
+                if (aasqParent[sqLogIdx][sqNode] < 0) {
+                    aasqParent[sqLogIdx + 1][sqNode] = -1;
+                } else {
+                    aasqParent[sqLogIdx + 1][sqNode] = aasqParent[sqLogIdx][aasqParent[sqLogIdx][sqNode]];
+                }
+            }
+        }
+    }
+
+    /**
+     * calculate lca of U and V
+     */
+    SQWORD lca(SQWORD sqU, SQWORD sqV)
+    {
+        /* u, v が同じ深さになるまで親を辿る */
+        if (asqDepth[sqU] > asqDepth[sqV]) {
+            swap(sqU, sqV);
+        }
+        for (SQWORD sqLogIdx = 0; sqLogIdx < MAX_LOG_NODES; sqLogIdx++) {
+            if (((asqDepth[sqV] - asqDepth[sqU]) >> sqLogIdx) & 0x1) {
+                sqV = aasqParent[sqLogIdx][sqV];
+            }
+        }
+        if (sqU == sqV) {
+            return sqU;
+        }
+
+        /* 二分探索でLCAを求める */
+        for (SQWORD sqLogIdx = MAX_LOG_NODES - 1; 0 <= sqLogIdx; sqLogIdx--) {
+            if (aasqParent[sqLogIdx][sqU] != aasqParent[sqLogIdx][sqV]) {
+                sqU = aasqParent[sqLogIdx][sqU];
+                sqV = aasqParent[sqLogIdx][sqV];
+            }
+        }
+
+        return aasqParent[0][sqU];
+    }
+};
+
+struct Query {
+    SQWORD sqIdx;
+    SQWORD sqColor;
+    SQWORD sqCost;
+    SQWORD sqCoeff;
+
+    Query(SQWORD idx, SQWORD col, SQWORD cost, SQWORD coeff)
+        :sqIdx(idx), sqColor(col), sqCost(cost), sqCoeff(coeff) {};
+};
+
+#define N_MAX_QUERY (100000)
+#define N_MAX_COLOR (100000)
+static SQWORD s_asqAns[N_MAX_QUERY];
+static SQWORD s_asqCostColor[N_MAX_COLOR + 1];
+static SQWORD s_asqNumColor[N_MAX_COLOR + 1];
+
+static void dfsQuery(
+    SQWORD sqNode, 
+    SQWORD sqParent, 
+    const vector<Query> pvecQuery[], 
+    TREE &stTree,
+    SQWORD asqCostColor[],
+    SQWORD asqNumColor[])
+{
+    /* exec query */
+    for (auto query: pvecQuery[sqNode]) {
+//        printf("%lld %lld %lld\n", query.sqIdx, query.sqColor, asqCostColor[query.sqColor]);
+        s_asqAns[query.sqIdx] -= query.sqCoeff * asqCostColor[query.sqColor];
+        s_asqAns[query.sqIdx] += query.sqCoeff * query.sqCost * asqNumColor[query.sqColor];
+    }
+
+    /* dfs */
+    for (auto edge: stTree.avecstEdge[sqNode]) {
+        if (edge.sqTo != sqParent) {
+            asqCostColor[edge.sqColor] += edge.sqCost;
+            asqNumColor[edge.sqColor]++;
+            dfsQuery(edge.sqTo, sqNode, pvecQuery, stTree, asqCostColor, asqNumColor);
+            asqCostColor[edge.sqColor] -= edge.sqCost;
+            asqNumColor[edge.sqColor]--;
+        }
+    }
+}
+
+
 int main(void)
 {
+    SQWORD sqInput_N = inputSQWORD();
+    SQWORD sqInput_Q = inputSQWORD();
+    static TREE    stTree(sqInput_N);
+
+    for (SQWORD sqIdx = 0; sqIdx < sqInput_N - 1; sqIdx++) {
+        SQWORD sqInput_a = inputSQWORD();
+        SQWORD sqInput_b = inputSQWORD();
+        SQWORD sqInput_c = inputSQWORD();
+        SQWORD sqInput_d = inputSQWORD();
+        stTree.retistEdge(sqInput_a, sqInput_b, sqInput_c, sqInput_d);
+    }
+    stTree.init(1);
+
+    static vector<Query> avecQuery[N_MAX_NODES + 1];
+    for (SQWORD sqQueryIdx = 0; sqQueryIdx < sqInput_Q; sqQueryIdx++) {
+        SQWORD sqInput_x = inputSQWORD();
+        SQWORD sqInput_y = inputSQWORD();
+        SQWORD sqInput_u = inputSQWORD();
+        SQWORD sqInput_v = inputSQWORD();
+
+        SQWORD sqLca = stTree.lca(sqInput_u, sqInput_v);
+        avecQuery[sqInput_u].emplace_back(sqQueryIdx, sqInput_x, sqInput_y, 1);
+        avecQuery[sqInput_v].emplace_back(sqQueryIdx, sqInput_x, sqInput_y, 1);
+        avecQuery[sqLca].emplace_back(sqQueryIdx, sqInput_x, sqInput_y, -2);       
+
+        s_asqAns[sqQueryIdx] = stTree.asqCost[sqInput_u]
+                                 + stTree.asqCost[sqInput_v]
+                                 - 2 * stTree.asqCost[sqLca];
+//        printf("orgcost[%lld %lld] %lld\n", sqInput_u, sqInput_v, s_asqAns[sqQueryIdx]);
+                                 
+    }
+
+    /* exec query with dfs */
+    dfsQuery(1, -1, avecQuery, stTree, s_asqCostColor, s_asqNumColor);
+
+    for (SQWORD sqQueryIdx = 0; sqQueryIdx < sqInput_Q; sqQueryIdx++) {
+        printf("%lld\n", s_asqAns[sqQueryIdx]);
+    }
 
     return 0;
 }
